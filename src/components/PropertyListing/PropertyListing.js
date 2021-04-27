ac0a7c7f5b2c525175c32292af527118e847416b
Mark Gun <mgun.sd@gmail.com>
2021-04-27 13:47:52 +0100

    second task sort and filter

--- src/components/PropertyListing/PropertyListing.js
+++ src/components/PropertyListing/PropertyListing.js
@@ -3,13 +3,14 @@ import PropertyCard from '../PropertyCard';
 import './PropertyListing.scss';
 import useProperties from '../../hooks/useProperties';
 
-const PropertyListing = () => {
-  const { properties, error, loading } = useProperties();
+const PropertyListing = ({ params }) => {
+  const { properties, error, loading } = useProperties(params);
 
   return (
     <div className="PropertyListing">
       {error && <h3>{error}</h3>}
       {loading && <h3>Loading...</h3>}
+
       {!error &&
         !loading &&
         properties.map((property, index) => (
--- src/hooks/useProperties.js
+++ src/hooks/useProperties.js
@@ -1,16 +1,17 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useProperties = () => {
+const useProperties = (params) => {
   const [properties, setProperties] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
   useEffect(() => {
+    console.log(`params: `, params);
     try {
       (async () => {
         const properties = await axios.get(
-          'http://localhost:3000/api/properties'
+          `http://localhost:3000/api/properties?sortBy=${params}`
         );
         setProperties(properties.data);
         setLoading(false);
@@ -19,7 +20,7 @@ const useProperties = () => {
       console.log(err);
       setError(err);
     }
-  }, []);
+  }, [params]);
   return { properties, error, loading };
 };
 
