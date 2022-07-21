63cea1a90cf565d70273472168e2a963d332765d
Michael Low <mike.low@rightmove.co.uk>
2022-07-21 01:22:47 +0100

    Fix merge errors

--- src/components/SortAndFilter/Dropdown/Dropdown.jsx
+++ src/components/SortAndFilter/Dropdown/Dropdown.jsx
@@ -2,31 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Dropdown.scss';
 
-const Dropdown = ({options, onChange, label}) => {
+const Dropdown = ({ options, onChange, label }) => {
     return (
         <div className="Dropdown">
             <label>
                 {label}
                 <select onChange={(e) => onChange(e.target.value)}>
-                    {
-                        options.map((option) => {
-                            return <option key={option} value={option}>{option}</option>
-                        })
-                    }
+                    {options.map((option) => {
+                        return (
+                            <option key={option} value={option}>
+                                {option}
+                            </option>
+                        );
+                    })}
                 </select>
             </label>
         </div>
     );
 };
 
-
 Dropdown.propTypes = {
-    options: PropTypes.arrayOf(PropTypes.oneOfType([
-        PropTypes.number,
-        PropTypes.string,
-    ])),
+    options: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.number, PropTypes.string])),
     onChange: PropTypes.func, // (selectedOption) => {}
-    label: PropTypes.string
+    label: PropTypes.string,
 };
 
 export default Dropdown;
